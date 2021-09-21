1b81630ec303655a8501589f192329b7c2d9f7d8
Nathan Petrangelo <nathan.petrangelo@amd.com>
2021-09-21 09:49:25 -0400

    Lesson 1 materials

---
+++ Lesson1-material/foo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define PI 3.14159
+
+int main (void) {
+    int x = 15;
+    if (x>10) {
+        printf("x is big\n");
+    } else {
+        printf("x is puny\n");
+    }
+    return 0;
+}
---
+++ Lesson1-material/point.c
@@ -0,0 +1,167 @@
+/*
+ *
+ * point.c - simple GLUT app that draws one frame with a single point at origin
+ *
+ * To build:  gcc -framework OpenGL -framework GLUT point.c -o point
+ *
+ */
+#ifndef GL_SILENCE_DEPRECATION
+#define GL_SILENCE_DEPRECATION
+#endif
+
+/*************************************************************************/
+/* header files                                                          */
+/*************************************************************************/
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <limits.h>
+#include <stdio.h>
+
+/*************************************************************************/
+/* defines                                                               */
+/*************************************************************************/
+#define repeat(x)       for( global_index = 0; global_index < (int)(x); global_index++ )
+
+/*************************************************************************/
+/* global variables                                                      */
+/*************************************************************************/
+int window_size = 400;
+int Mojave_WorkAround = 0;
+int draw_one_frame = 1;
+int global_index;
+
+/*************************************************************************/
+/* utility functions                                                     */
+/*************************************************************************/
+/*
+ * random_float()
+ */
+float random_float( float low, float high )
+{
+    return( (float)(low + (rand()/(float)RAND_MAX)*(high - low)) );
+}
+
+/*
+ * set_color()
+ */
+void set_color( float r, float g, float b, float a )
+{
+    glColor4f( r, g, b, a );
+}
+
+/*
+ * draw_point()
+ */
+void draw_point( float x, float y )
+{
+    /*
+     * draw points
+     */
+    glBegin(GL_POINTS);
+        glVertex2f( x, y );
+    glEnd();
+
+}
+
+/*
+ * my_drawing
+ *
+ *    set_color( r, g, b, a );
+ *    draw_point( x, y );
+ */
+void my_drawing( void )
+{
+    float x = 200;
+    float y = 100;
+
+
+    set_color( 0, 1, 0, 0 );
+    draw_point( x, y );
+}
+
+/*************************************************************************/
+/* GLUT functions                                                        */
+/*************************************************************************/
+/*
+ * display routine
+ */
+void display(void)
+{
+    if( Mojave_WorkAround )
+    {
+        glutReshapeWindow(2 * window_size,2 * window_size);//Necessary for Mojave. Has to be different dimensions than in glutInitWindowSize();
+        Mojave_WorkAround = 0;
+    }
+
+    if( draw_one_frame == 0 )
+        return;
+	
+    /*
+     * clear color buffer
+     */
+    glClear(GL_COLOR_BUFFER_BIT );
+
+    my_drawing();
+    
+    /*
+     * show results
+     */
+    glutSwapBuffers();
+    glutPostRedisplay();//Necessary for Mojave.
+
+    draw_one_frame = 0;
+}
+
+/*
+ * Key routine
+ */
+static void Key(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+        case 'a':       draw_one_frame = 1;     glutPostRedisplay();    break;
+        case 'q':       exit(0);                                        break;
+        case '\033':    exit(0);                                        break;
+    }
+}
+
+/*
+ * main function
+ */
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    srand(time(NULL));
+
+    /*
+     * create window
+     */
+    glutInitWindowSize(window_size,window_size);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutCreateWindow("My OpenGL Window");
+
+    /*
+     * setup display(), Key() funtions
+     */
+    glutDisplayFunc(display);
+    glutKeyboardFunc(Key);
+
+    /*
+     * setup OpenGL state
+     */
+    glClearColor(0.0,0.0,0.0,1.0);
+    gluOrtho2D(-window_size,window_size,-window_size,window_size);
+    glPointSize(2.0);
+    glColor4f(1.0,0.0,0.0,1.0);
+
+    /*
+     * start loop that calls display() and Key() routines
+     */
+    glutMainLoop();
+
+    return 0;
+}

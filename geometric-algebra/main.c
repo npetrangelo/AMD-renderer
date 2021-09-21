342701e27543ac08d5432c1d938ef71fbc1ea1c0
Nathan Petrangelo <nathan.petrangelo@amd.com>
2021-09-21 15:49:25 -0400

    Added nvectors

--- geometric-algebra/main.c
+++ geometric-algebra/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "vector.h"
+#include "nvector.h"
 
 int main ( void ) {
     vector* v1 = vector3(1.0, 0.0, 0.0);
@@ -10,5 +11,15 @@ int main ( void ) {
         printf("Addition error\n");
     } else {
         printf("len=%d, {%f, %f, %f}\n", v->len, v->arr[0], v->arr[1], v->arr[2]);
+    }
+
+    nvector* nv = bivector(v1, v2);
+    if (!nv) {
+        printf("Bivector error\n");
+    } else {
+        printf("Bivector len=%d\n", nv->len);
+        printf("{%f, %f, %f}, {%f, %f, %f}\n",
+            nv->arr[0]->arr[0], nv->arr[0]->arr[1], nv->arr[0]->arr[2],
+            nv->arr[1]->arr[0], nv->arr[1]->arr[1], nv->arr[1]->arr[2]);
     }
 }

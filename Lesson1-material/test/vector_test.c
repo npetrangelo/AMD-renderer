ad2eb37a7a39764b0bed9fc9501fd3cdc99e90cb
Nathan Petrangelo <nathan.petrangelo@amd.com>
2021-11-11 17:12:26 -0500

    Test lookat() and half angle functions

--- Lesson1-material/test/vector_test.c
+++ Lesson1-material/test/vector_test.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include "../utility.h"
 #include "../vector.h"
 #include "../quaternion.h"
+#include "../camera.h"
+#include "../buffer.h"
 
 #define PI 3.141592653589
 
@@ -19,7 +23,25 @@ void testRotate(float v[4], float angle) {
     printf("[%f %f %f]\n", result[0], result[1], result[2]);
 }
 
+void testLookAt() {
+    float pos[4] = {0.0, 0.0, 3.0, 0.0};
+    Camera *cam = make_camera(500.0, pos);
+    float origin[4] = {0.0};
+    look_at(cam, origin);
+    printf("cam->q = [%f, %f, %f, %f]\n", cam->q[0], cam->q[1], cam->q[2], cam->q[3]);
+}
+
+void test_half_angle_cos() {
+    printf("%f = %f?\n", 0.0, half_angle_cos(-1.0));
+    printf("%f = %f?\n", sqrt(2.0)/2.0, half_angle_cos(0.0));
+
+    printf("%f = %f?\n", -sqrt(2.0)/2.0, half_angle_sin(0.0, -1.0));
+    printf("%f = %f?\n", 0.0, half_angle_sin(1.0, 0.0));
+}
+
 int main(int argc, char **argv) {
     float v[4] = {1.0, 0.0, 0.0, 0.0};
     testRotate(v, PI/4.0);
+    test_half_angle_cos();
+    testLookAt();
 }
